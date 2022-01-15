7fe19065b9c4439fbe0135b986cb47a212db71b9
10746 <1074650916@qq.com>
2022-01-15 18:38:18 +0800

    2020/01/15

--- 03QuitApp.py
+++ 03QuitApp.py
@@ -1,6 +1,8 @@
 import sys
+
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, \
-    QApplication, QPushButton, QWidget
+    QApplication, QPushButton, QWidget, QToolTip
 
 
 class QuitAPP(QMainWindow):
@@ -11,6 +13,12 @@ class QuitAPP(QMainWindow):
 
         # 添加按钮
         self.button1 = QPushButton('退出程序的按钮')
+        # 设置按钮字体和大小
+        self.button1.setFont(QFont('SansSerif', 16))
+        # 添加按钮提示信息
+        self.button1.setToolTip('这是按钮的提示信息')
+        # 设置提示信息字体和大小
+        QToolTip.setFont(QFont('SansSerif', 24))
         # 将信号与槽关联
         self.button1.clicked.connect(self.onClick_Button)
         # 创建一个水平布局
@@ -29,7 +37,7 @@ class QuitAPP(QMainWindow):
         sender = self.sender()
         # sender.text() 获取信号对象文本
         print(sender.text() + '被按下')
-        # 获取instance() 获取单例对象(获取当前窗口)
+        # 获取instance() 获取单例对象(获取当前窗口)，单例：
         app = QuitAPP.instance()
         app.quit()
 
@@ -40,6 +48,9 @@ if __name__ == '__main__':
 
     # 窗口实例化
     main = QuitAPP()
+
+    # 设置图标
+    app.setWindowIcon(QIcon('./01.png'))
 
     # 展示窗口
     main.show()
